398cde0e92b14fbb5234041a8fcdc5a9e7dd8a61
kanwalsamuel <160218038+kanwalsamuel@users.noreply.github.com>
2024-03-24 23:34:35 +0500

    Add files via upload
    cli game

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10);
+const answer = await inquirer.prompt([{
+        name: "userguessNunber",
+        type: "number",
+        message: "please guess a number between 1 to 10"
+    }]);
+if (answer.userguessNunber === randomNumber) {
+    console.log("congratulations you guess right number");
+}
+else {
+    console.log("you guess wrong number");
+}
