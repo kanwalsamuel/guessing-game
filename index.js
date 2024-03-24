204563e0108b008c8e96eef1184d3f31b3e7635b
kanwalsamuel <160218038+kanwalsamuel@users.noreply.github.com>
2024-03-25 00:25:30 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = Math.floor(Math.random() * 10);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([{
         name: "userguessNunber",
         type: "number",
-        message: "please guess a number between 1 to 10"
+        message: "please guess a number between 1 to 6:"
     }]);
 if (answer.userguessNunber === randomNumber) {
-    console.log("congratulations you guess right number");
+    console.log("congratulations! you guessed a right number.");
 }
 else {
-    console.log("you guess wrong number");
+    console.log("you guessed a wrong number.");
 }
